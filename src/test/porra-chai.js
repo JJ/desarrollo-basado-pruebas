4e7a3073010932f0e232a4a9e226beecddd6b64a
JJ Merelo <jjmerelo@gmail.com>
2019-09-27 12:12:17 +0200

    Arregla y :see_no_evil:

---
+++ src/test/porra-chai.js
@@ -0,0 +1,19 @@
+var assert = require("chai").assert,
+    apuesta = require(__dirname+"/../Apuesta.js");
+
+console.log(assert);
+describe('Apuesta con Chai', function(){
+    // Testea que se haya cargado bien la librería
+    describe('Carga', function(){
+	it('should be loaded', function(){
+	    assert.ok(apuesta, "Cargado");
+	});
+	
+    });
+    describe('Crea', function(){
+	it('should create apuestas correctly', function(){
+	    var nueva_apuesta = new apuesta.Apuesta('Polopos','Alhama','2-3');
+	    assert.equal(nueva_apuesta.as_string(), "Polopos: Alhama - 2-3","Creado");
+	});
+    });
+});
